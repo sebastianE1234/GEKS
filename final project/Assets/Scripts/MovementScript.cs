75ec55284b410eefcf08bccca91705ab4fa4b3b8
sebastianE1234 <evanss5@go.lisd.net>
2025-04-04 14:47:50 -0500

    made script changes

--- final project/Assets/Scripts/MovementScript.cs
+++ final project/Assets/Scripts/MovementScript.cs
@@ -16,11 +16,15 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    void FixedUpdate()
+    {
+        // Ground Check
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        Debug.Log("grounded? "+ isGrounded);
+    }
+
     void Update()
     {
-
-        Debug.Log("Grounded: " + isGrounded);
-
         float moveInput = 0f;
         bool jumpPressed = false;
 
@@ -28,7 +32,9 @@ public class PlayerMovement : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.LeftArrow)) moveInput = -1f;
             if (Input.GetKey(KeyCode.RightArrow)) moveInput = 1f;
+
             jumpPressed = Input.GetKeyDown(KeyCode.UpArrow);
+
         }
         else
         {
@@ -40,13 +46,21 @@ public class PlayerMovement : MonoBehaviour
         // Horizontal Movement
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
-        // Ground Check
-        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
-
         // Jumping
-        if (jumpPressed && isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+
+
+    void OnDrawGizmosSelected()
+    {
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireSphere(groundCheck.position, 0.2f);
         }
     }
 }
