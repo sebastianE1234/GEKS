a1ede5822b6d3a97d8f491b02c2ddaea11c73810
sebastianE1234 <evanss5@go.lisd.net>
2025-04-04 12:51:48 -0500

    updated script

--- final project/Assets/MovementScript.cs
+++ final project/Assets/MovementScript.cs
@@ -6,16 +6,13 @@ public class PlayerMovement : MonoBehaviour
     public float jumpForce = 7f;
     public Transform groundCheck;
     public LayerMask groundLayer;
-    public int maxExtraJumps = 5; // The number of jumps added when landing
 
     private Rigidbody2D rb;
     private bool isGrounded;
-    private int jumpCount;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        jumpCount = maxExtraJumps; // Start with 2 extra jumps
     }
 
     void Update()
@@ -25,20 +22,12 @@ public class PlayerMovement : MonoBehaviour
         rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
 
         // Ground Check
-        bool wasGrounded = isGrounded;
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
 
-        // Add 2 jumps when landing
-        if (isGrounded && !wasGrounded)
-        {
-            jumpCount += maxExtraJumps; // Add 2 extra jumps when landing
-        }
-
         // Jumping
-        if (Input.GetKeyDown(KeyCode.Space) && jumpCount > 0)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
-            jumpCount--;
         }
     }
 }
