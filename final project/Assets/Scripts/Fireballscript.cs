2619caf96503e23fb76a6c5932c61a1d3e8878a3
sebastianE1234 <evanss5@go.lisd.net>
2025-04-08 14:57:25 -0500

    FINALLY GOT THE FIREBALLS TO SHOOT!!!!!

--- final project/Assets/Scripts/Fireballscript.cs
+++ final project/Assets/Scripts/Fireballscript.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
 
-public class Fireballscript
+public class SimpleFireballShooter : MonoBehaviour
 {
-    
+    public GameObject FireballPrefab;  // Drag your Fireball prefab here
+    public Transform firePoint;        // Where the fireball spawns
+    public float fireballSpeed = 10f;  // Speed of the fireball
+
+    public enum PlayerID { PlayerOne, PlayerTwo }
+    public PlayerID playerID;
+    void Update()
+    {
+        if (playerID == PlayerID.PlayerOne && Input.GetKeyDown(KeyCode.B))
+        {
+            Shoot(Vector2.right);
+        }
+
+        if (playerID == PlayerID.PlayerTwo && Input.GetKeyDown(KeyCode.Space))
+        {
+
+            Shoot(Vector2.left);
+        }
+    }
+
+    void Shoot(Vector2 direction)
+    {
+        GameObject fireball = Instantiate(FireballPrefab, firePoint.position, Quaternion.identity);
+        Rigidbody2D rb = fireball.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.linearVelocity = direction.normalized * fireballSpeed;
+        }
+    }
 }
