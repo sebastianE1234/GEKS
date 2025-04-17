8e66d005b508c9832fd8d0debaa2dc818a00994b
EJT-Git <elihteran@gmail.com>
2025-04-17 15:03:27 -0500

    summary (required)

---
+++ final project/Assets/Scripts/Projectile.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public Rigidbody2D projectileRb;
+    public float speed;
+
+    public float projectileLife;
+    public float projectileCount;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        projectileCount = projectileLife;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        projectileCount -= Time.deltaTime;
+        if(projectileCount <= 0)  
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    [System.Obsolete]
+    private void FixedUpdate()
+    {
+        projectileRb.velocity = new Vector2(speed, projectileRb.velocity.y);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "P2")
+        {
+            Destroy(collision.gameObject);
+        }
+        Destroy(gameObject);
+    }
+}
